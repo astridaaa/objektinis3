f310cd1cd3f89cca055855117311f98df2548d76
Astrida Jablonskytė <astrida789@gmail.com>
2025-05-05 00:28:47 +0300

    finished testing rule of five

--- unitTesting.cpp
+++ unitTesting.cpp
@@ -2,8 +2,39 @@
 #include "catch2/catch.hpp"
 #include "main.h"
 
+//rule of five testavimas naudojant catch2
+
 TEST_CASE("Copy constructor", "[Copy][constructor]") {
     Stud s("Astrida", "Jablonskyte", 10, {10, 10, 10});
     Stud s1(s);
     REQUIRE(s==s1);}
+
+TEST_CASE("Copy assignment", "[Copy][assignment]"){
+    Stud s("Astrida", "Jablonskyte", 10, {10, 10, 10});
+    Stud s1=s;
+    REQUIRE(s1==s);
+}
+
+TEST_CASE("Move constructor", "[Move][constructor]"){
+    Stud s("Astrida", "Jablonskyte", 10, {10, 10, 10});
+    Stud s1(std::move(s));
+    REQUIRE(s1==Stud("Astrida", "Jablonskyte", 10, {10, 10, 10}));
+    REQUIRE(s!=s1);
+}
+
+TEST_CASE("Move assignment", "[Move][assignment]"){
+    Stud s("Astrida", "Jablonskyte", 10, {10, 10, 10}), s1;
+    s1 = std::move(s);
+    REQUIRE(s1==Stud("Astrida", "Jablonskyte", 10, {10, 10, 10}));
+    REQUIRE(s1!=s);
+}
+
+TEST_CASE("Destructor", "[Destructor]"){
+    Stud *s = new Stud("Astrida", "Jablonskyte", 10, {10, 10, 10});
+    s->~Stud();
+    REQUIRE(s->getND().empty());
+    operator delete(s);
+}
+
+
 
