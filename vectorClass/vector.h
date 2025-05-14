2ad422c1685f520ce4ebcf1e6495c23c80a31a14
Astrida Jablonskytė <astrida789@gmail.com>
2025-05-14 17:51:33 +0300

    realized rule of 5 for Vector class

--- vectorClass/vector.h
+++ vectorClass/vector.h
@@ -17,11 +17,42 @@ template <typename T> class Vector{
     Vector(size_t Size): size(Size), capacity(Size){
         vec = new T[capacity];
     }
-    //vector initialization dar galimai testavimui reikes bet siaip visur puchbacka naudoju
 
-    //destructor
-    ~Vector(){}
+    // TODO: vector initialization dar galimai testavimui reikes bet siaip visur puchbacka naudoju
+
+    ~Vector(){capacity=0; size=0; delete[] vec;}
+
+    //copy constructor | rule of 5
+    Vector(const Vector& v): vec(v.vec), capacity(v.capacity), size(v.vec) {}
+    
+    //copy assignment operator | rule of 5
+    Vector &operator=(const Vector& v){
+        if(this!=&v){
+            vec=v.vec;
+            capacity=v.capacity;
+            size=v.size;
+        }return *this;
+    }
+    
+    //move constructor | rule of 5 (galimai neveiks destruktorius ir reikes clear nes pointes involved)
+    Vector(Vector &&v) : vec(std::move(v.vec)), capacity(std::move(v.capacity)), size(std::move(v.size)){~Vector();}
+
+    //move assignment operator | rule of 5
+    Vector &operator=(Vector &&v){
+        if(this!=v){
+            vec=std::move(v.vec);
+            capacity=std::move(v.capacity);
+            size=std::move(v.size);
+        }return *this;
+        ~Vector();
+    }
+
 
 }
 
 #endif
+
+
+bool empty(){
+    return size == 0;
+}
