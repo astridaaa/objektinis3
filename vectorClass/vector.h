103595f24820291b743b9621e3a8192483d78b8c
Astrida Jablonskytė <astrida789@gmail.com>
2025-05-14 17:06:44 +0300

    added few constructors

--- vectorClass/vector.h
+++ vectorClass/vector.h
@@ -1,6 +1,27 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-//vector class impelmentacija: member functions; non-member functions; member types
+template <typename T> class Vector{
+
+    private:
+    T* vec; // saugo adresa vektoriaus 
+    size_t capacity; // size_t - unsigned integer type
+    size_t size;
+
+    public:
+
+    //constructor
+    Vector() = vec(nullptr), capacity(0), size(0) {}
+
+    //with initial size
+    Vector(size_t Size): size(Size), capacity(Size){
+        vec = new T[capacity];
+    }
+    //vector initialization dar galimai testavimui reikes bet siaip visur puchbacka naudoju
+
+    //destructor
+    ~Vector(){}
+
+}
 
 #endif
